c5afbb35937a440cfc0739125e3f6d55dd740943
Humoyunmirzo2012 <webdevoper0509@gmail.com>
2024-08-01 18:31:14 +0500

    js-vazifa

---
+++ js/app.js
@@ -0,0 +1,83 @@
+// let num1 = 10;
+// let num2 = 5;
+
+// let yigindi = num1 + num2;
+// console.log(`Yig'indi: ${yigindi}`);
+
+// let ayirma = num1 - num2;
+// console.log(`Ayirma: ${ayirma}`);
+
+// let ko'paytma = num1 * num2;
+// console.log(`Ko'paytma: ${ko'paytma}`);
+
+// let bo'linma = num1 / num2;
+// console.log(`Bo'linma: ${bo'linma}`);
+
+// let qoldiq = num1 % num2;
+// console.log(`Qoldiq: ${qoldiq}`);
+
+// let a = 10;
+// let b = 20;
+
+// a += b; // a = a + b
+// console.log(`a += dan keyin: ${a}`);
+
+// a -= b; // a = a - b
+// console.log(`a -= dan keyin: ${a}`);
+
+// a *= b; // a = a * b
+// console.log(`a *= dan keyin: ${a}`);
+
+// a /= b; // a = a / b
+// console.log(`a /= dan keyin: ${a}`);
+
+// a %= b; // a = a % b
+// console.log(`a %= dan keyin: ${a}`);
+
+// let x = 15;
+// let y = 20;
+
+// console.log(`x == y: ${x == y}`);
+// console.log(`x != y: ${x != y}`);
+// console.log(`x === y: ${x === y}`);
+// console.log(`x !== y: ${x !== y}`);
+// console.log(`x > y: ${x > y}`);
+// console.log(`x < y: ${x < y}`);
+// console.log(`x >= y: ${x >= y}`);
+// console.log(`x <= y: ${x <= y}`);
+
+// let a = true;
+// let b = false;
+
+// console.log(`a && b: ${a && b}`);
+// console.log(`a || b: ${a || b}`);
+// console.log(`!a: ${!a}`);
+// console.log(`!b: ${!b}`);
+
+// let sanash = 0;
+
+// sanash++;
+// console.log(`sanash inkremendan keyin: ${sanash}`);
+
+// sanash--;
+// console.log(`sanash dekrementdan keyin: ${sanash}`);
+
+// let sanash = 0;
+
+// sanash++;
+// console.log(`sanash inkremendan keyin: ${sanash}`);
+
+// sanash--;
+// console.log(`sanash dekrementdan keyin: ${sanash}`);
+
+// let a = 10;
+// let b = 5;
+// let c = 20;
+
+// // Murakkab arifmetik ifoda
+// let arifmetikNatija = (a + b) * c / (a - b) + (c % b);
+// console.log(`Murakkab arifmetik ifoda natijasi: ${arifmetikNatija}`);
+
+// // Murakkab mantiqiy ifoda
+// let mantiqiyNatija = (a > b && c > a) || (a % b === 0 && c % a !== 0);
+// console.log(`Murakkab mantiqiy ifoda natijasi: ${mantiqiyNatija}`);
